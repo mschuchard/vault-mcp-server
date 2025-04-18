8fc3c5f30c5504341cefaac3ca57cc50a9310d02
Matthew Schuchard <8029687+mschuchard@users.noreply.github.com>
2025-04-18 14:41:16 -0400

    utilize singular client object for vault interfacing

--- main.py
+++ main.py
@@ -55,10 +55,10 @@ def acl_policies() -> json:
 ## auth
 ### general
 @mcp.tool(name='Enable Authentication Engine')
-def auth_engine_enable(engine: str, mount: str = None) -> bool:
+def auth_engine_enable(ctx: Context, engine: str, mount: str = None) -> bool:
     """enable a vault authentication engine"""
     try:
-        client.client().sys.enable_auth_method(method_type=engine, path=mount)
+        ctx.request_context.lifespan_context['client'].sys.enable_auth_method(method_type=engine, path=mount)
 
         return True
     except Exception:
@@ -66,10 +66,10 @@ def auth_engine_enable(engine: str, mount: str = None) -> bool:
 
 
 @mcp.tool(name='Disable Authentication Engine')
-def auth_engine_disable(mount: str) -> bool:
+def auth_engine_disable(ctx: Context, mount: str) -> bool:
     """disable a vault auth engine"""
     try:
-        client.client().sys.disable_auth_method(path=mount)
+        ctx.request_context.lifespan_context['client'].sys.disable_auth_method(path=mount)
 
         return True
     except Exception:
@@ -86,10 +86,10 @@ async def auth_engine_list(ctx: Context) -> dict:
 ## secret engines
 ### general
 @mcp.tool(name='Enable Secret Engine')
-def secret_engine_enable(engine: str, mount: str = None) -> bool:
+def secret_engine_enable(ctx: Context, engine: str, mount: str = None) -> bool:
     """enable a vault secret engine"""
     try:
-        client.client().sys.enable_secrets_engine(backend_type=engine, path=mount)
+        ctx.request_context.lifespan_context['client'].sys.enable_secrets_engine(backend_type=engine, path=mount)
 
         return True
     except Exception:
@@ -97,10 +97,10 @@ def secret_engine_enable(engine: str, mount: str = None) -> bool:
 
 
 @mcp.tool(name='Disable Secret Engine')
-def secret_engine_disable(mount: str) -> bool:
+def secret_engine_disable(ctx: Context, mount: str) -> bool:
     """disable a vault secret engine"""
     try:
-        client.client().sys.disable_secrets_engine(path=mount)
+        ctx.request_context.lifespan_context['client'].sys.disable_secrets_engine(path=mount)
 
         return True
     except Exception:
@@ -116,9 +116,9 @@ async def secret_engine_list(ctx: Context) -> dict:
 
 ### kv2
 @mcp.tool(name='KV2 Write')
-def kv2_write(mount: str = 'secret', path: str = '', secret: dict = {}) -> dict:
+def kv2_write(ctx: Context, mount: str = 'secret', path: str = '', secret: dict = {}) -> dict:
     """write a kv2 secret to vault"""
-    return client.client().secrets.kv.v2.create_or_update_secret(
+    return ctx.request_context.lifespan_context['client'].secrets.kv.v2.create_or_update_secret(
         mount_point=mount,
         path=path,
         secret=secret,
@@ -126,10 +126,10 @@ def kv2_write(mount: str = 'secret', path: str = '', secret: dict = {}) -> dict:
 
 
 @mcp.tool(name='KV2 Delete')
-def kv2_delete(mount: str = 'secret', path: str = '') -> bool:
+def kv2_delete(ctx: Context, mount: str = 'secret', path: str = '') -> bool:
     """delete a kv2 secret from vault"""
     try:
-        client.client().secrets.kv.delete_metadata_and_all_versions(mount_point=mount, path=path)
+        ctx.request_context.lifespan_context['client'].secrets.kv.delete_metadata_and_all_versions(mount_point=mount, path=path)
 
         return True
     except Exception:
@@ -137,24 +137,24 @@ def kv2_delete(mount: str = 'secret', path: str = '') -> bool:
 
 
 @mcp.tool(name='KV2 Read')
-def kv2_read(mount: str = 'secret', path: str = '') -> dict:
+def kv2_read(ctx: Context, mount: str = 'secret', path: str = '') -> dict:
     """read a kv2 secret from a vault"""
-    return client.client().secrets.kv.read_secret_version(mount_point=mount, path=path)['data']
+    return ctx.request_context.lifespan_context['client'].secrets.kv.read_secret_version(mount_point=mount, path=path)['data']
 
 
 @mcp.tool(name='KV2 List')
-def kv2_list(mount: str = 'secret', path: str = '') -> list:
+def kv2_list(ctx: Context, mount: str = 'secret', path: str = '') -> list:
     """list the kv2 secrets in vault"""
-    return client.client().secrets.kv.v2.list_secrets(mount_point=mount, path=path)['data']['keys']
+    return ctx.request_context.lifespan_context['client'].secrets.kv.v2.list_secrets(mount_point=mount, path=path)['data']['keys']
 
 
 ## policies
 ### general
 @mcp.tool(name='Policy Write')
-def policy_write(name: str, policy: dict[str, dict[str, dict[str, list[str]]]]) -> bool:
+def policy_write(ctx: Context, name: str, policy: dict[str, dict[str, dict[str, list[str]]]]) -> bool:
     """write a acl policy to vault"""
     try:
-        client.client().sys.create_or_update_acl_policy(name=name, policy=policy)
+        ctx.request_context.lifespan_context['client'].sys.create_or_update_acl_policy(name=name, policy=policy)
 
         return True
     except Exception:
@@ -162,10 +162,10 @@ def policy_write(name: str, policy: dict[str, dict[str, dict[str, list[str]]]]) 
 
 
 @mcp.tool(name='Policy Delete')
-def policy_delete(name: str) -> bool:
+def policy_delete(ctx: Context, name: str) -> bool:
     """delete a acl policy from vault"""
     try:
-        client.client().sys.delete_acl_policy(name=name)
+        ctx.request_context.lifespan_context['client'].sys.delete_acl_policy(name=name)
 
         return True
     except Exception:
@@ -173,9 +173,9 @@ def policy_delete(name: str) -> bool:
 
 
 @mcp.tool(name='Policy Read')
-def policy_read(name: str) -> dict:
+def policy_read(ctx: Context, name: str) -> dict:
     """read a acl policy from vault"""
-    return client.client().sys.read_acl_policy(name=name)
+    return ctx.request_context.lifespan_context['client'].sys.read_acl_policy(name=name)
 
 
 @mcp.tool(name='Policy List')
