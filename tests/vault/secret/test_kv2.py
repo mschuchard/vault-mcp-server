38208efe6a001e2f267bfa865ea6c34d641b56c4
Matthew Schuchard <8029687+mschuchard@users.noreply.github.com>
2025-08-04 11:09:57 -0400

    update secret unit tests for new version of mcp

--- tests/vault/secret/test_kv2.py
+++ tests/vault/secret/test_kv2.py
@@ -10,28 +10,28 @@ async def test_kv2() -> None:
     async with utils.mcp_client() as client:
         # create_update
         result = await client.call_tool(name='kv2-create-or-update', arguments={'path': 'mysecret', 'secret': {'foo': 'bar', 'baz': 'bat'}})
-        assert result[0]
+        assert result.data.get('version') == 1
 
         # list
         result = await client.call_tool(name='kv2-list')
-        assert result[0]
+        assert result.data == ['mysecret']
 
         # read
         result = await client.call_tool(name='kv2-read', arguments={'path': 'mysecret'})
-        assert result[0]
+        assert result.data == {'foo': 'bar', 'baz': 'bat'}
 
         # patch
         result = await client.call_tool(name='kv2-patch', arguments={'path': 'mysecret', 'secret': {'foobar': 'bazbat'}})
-        assert result[0]
+        assert result.data.get('version') == 2
 
         # metadata
         result = await client.call_tool(name='kv2-metadata-and-versions', arguments={'path': 'mysecret'})
-        assert result[0]
+        assert result.data.get('current_version') == 2
 
         # delete
         result = await client.call_tool(name='kv2-delete', arguments={'path': 'mysecret'})
-        assert result[0]
+        assert result.data.get('success') is True
 
         # undelete
-        result = await client.call_tool(name='kv2-undelete', arguments={'versions': [1], 'path': 'mysecret'})
-        assert result[0]
+        result = await client.call_tool(name='kv2-undelete', arguments={'versions': [1, 2], 'path': 'mysecret'})
+        assert result.data.get('success') is True
--- tests/vault/secret/test_transit.py
+++ tests/vault/secret/test_transit.py
@@ -18,29 +18,29 @@ async def test_transit() -> None:
 
         # create
         result = await client.call_tool(name='transit-engine-encryption-key-create', arguments={'name': 'mykey'})
-        assert result[0]
+        assert result.data.get('name') == 'mykey'
 
         # update config
         result = await client.call_tool(
             name='transit-engine-encryption-key-update-config', arguments={'name': 'mykey', 'deletion_allowed': True, 'exportable': True}
         )
-        assert result[0]
+        assert result.data.get('deletion_allowed') is True
 
         # read
         result = await client.call_tool(name='transit-engine-encryption-key-read', arguments={'name': 'mykey'})
-        assert result[0]
+        assert result.data.get('name') == 'mykey'
 
         # list
         result = await client.call_tool(name='transit-engine-encryption-keys-list')
-        assert result[0]
+        assert result.data == ['mykey']
 
         # rotate
         result = await client.call_tool(name='transit-engine-encryption-key-rotate', arguments={'name': 'mykey'})
-        assert result[0]
+        assert result.data.get('name') == 'mykey'
 
         # encrypt
         result = await client.call_tool(name='transit-engine-encrypt-plaintext', arguments={'name': 'mykey', 'text': 'helloworld'})
-        assert result[0]
+        assert result.is_error is False
 
         # decrypt: TODO need to parse `result` to obtain ciphertext for decryption
         # result = await client.call_tool(name='transit-engine-decrypt-ciphertext', arguments={'name': 'mykey', 'text': ''})
@@ -48,8 +48,8 @@ async def test_transit() -> None:
 
         # generate
         result = await client.call_tool(name='transit-engine-generate-random-bytes', arguments={'num_bytes': 16})
-        assert result[0]
+        assert result.is_error is False
 
         # delete
         result = await client.call_tool(name='transit-engine-encryption-key-delete', arguments={'name': 'mykey'})
-        assert result[0]
+        assert result.data.get('success') is True
